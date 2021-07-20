83bc254944170a4289e8286b544f09451a22b8f4
portable93 <87625622+portable93@users.noreply.github.com>
2021-07-20 11:01:54 +0900

    Add files via upload

--- GetRequestUrl.py
+++ GetRequestUrl.py
@@ -1,21 +1,26 @@
+# <Summary> 
+# - URL 요청 클래스ㅇ
+# <remarks> - Client ID, Secret 변경가능
+
 import urllib.request
 import datetime
-import json
 
-client_id = "i0rOFq7sJon3SVankcLB"
-client_secret = "iNHdUrvzYV"
+# <Summary> - URL 요청 메서드
+# <param> - url : URL주소, clientId : 클라이언트 아이디, clientSecret : 클라이언트 시크릿
+# <return> - 리턴값 내용
 
-def get_request_url(url): 
-    req = urllib.request.Request(url) 
-    req.add_header("X-Naver-Client-Id", client_id)
-    req.add_header("X-Naver-Client-Secret", client_secret)
+def GetRequestUrl(url, client_Id, client_Secret): # 데이터 요청하여 가져오기 - 크럴러 작업
+
+
+    req = urllib.request.Request(url)
+    req.add_header("X-Naver-Client-Id", client_Id)
+    req.add_header("X-Naver-Client-Secret", client_Secret)
 
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
             print("[%s] Url 요청 성공 : " % datetime.datetime.now())    
             return response.read().decode('utf-8')
-    
     except Exception as ex:
         print(ex)
         print("[%s] 오류 : %s " % datetime.datetime.now(), url)
