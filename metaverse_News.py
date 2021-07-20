f343846517c2fe48b1cb63cb1332a29332906bf3
jinsu89 <87625593+jinsu89@users.noreply.github.com>
2021-07-20 09:41:30 +0900

    Create metaverse_News.py

---
+++ metaverse_News.py
@@ -0,0 +1,22 @@
+import urllib.request
+import datetime
+import json
+\
+client_id = "i0rOFq7sJon3SVankcLB"
+client_secret = "iNHdUrvzYV"
+
+def get_request_url(url): 
+    req = urllib.request.Request(url) 
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url 요청 성공 : " % datetime.datetime.now())    
+            return response.read().decode('utf-8')
+    
+    except Exception as ex:
+        print(ex)
+        print("[%s] 오류 : %s " % datetime.datetime.now(), url)
+        return None
