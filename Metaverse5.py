b782bef50e0d16b493641bf96dd41bae466cadc9
seulkibae <87625547+seulkibae@users.noreply.github.com>
2021-07-23 15:14:31 +0900

    Add files via upload

--- Metaverse5.py
+++ Metaverse5.py
@@ -1,9 +1,20 @@
+# <Summary>
+# NAVER 검색 API를 이용해 카테고리별 데이터 정제
+# <Remarks>
+# 
+# </Remakrs>
+
 import urllib.request
 import datetime
 import json
 import pandas as pd
 import requests
 
+# <Summary>
+# NAVER API 호출, 클라이언트 연결
+# <Remarks>
+# 클라이언트 id & secret 입력(검색허용량 25,000회/일)
+# </Remakrs>
 def GetNaverSearchResult(searchNode, searchText, pageStart, display):
     baseurl = "https://openapi.naver.com/v1/search/"
     nodedata = "%s.json" % searchNode
@@ -17,6 +28,8 @@ def GetNaverSearchResult(searchNode, searchText, pageStart, display):
                                         "X-Naver-Client-Secret" :"iNHdUrvzYV"})
     return searchReq.json()
 
+# <Summary>
+# 카테고리 별(news) 필드값 명명
 def GetNewsDateChange(data, jsonResult):
     resultTitle = data['title'].replace("<b>", "*").replace("</b>", "*")
     resultDesc = data['description'].replace("<b>", "*").replace("</b>", "*")
@@ -32,6 +45,8 @@ def GetNewsDateChange(data, jsonResult):
                         '날짜': changeDateResult})
     return 
 
+# <Summary>
+# 카테고리 별(blog) 필드값 명명
 def GetBlogDateChange(data, jsonResult):
     resultTitle = data['title'].replace("<b>", "*").replace("</b>", "*")
     resultDesc = data['description'].replace("<b>", "*").replace("</b>", "*")
@@ -45,6 +60,8 @@ def GetBlogDateChange(data, jsonResult):
                         '날짜': resultDate})
     return 
 
+# <Summary>
+# 카테고리 별(cafe) 필드값 명명
 def GetCafeDateChange(data, jsonResult):
     resultTitle = data['title'].replace("<b>", "*").replace("</b>", "*")
     resultDesc = data['description'].replace("<b>", "*").replace("</b>", "*")
@@ -56,46 +73,49 @@ def GetCafeDateChange(data, jsonResult):
                         'cafeurl': resultCafeurl})
     return 
 
+# <Summary>
+# 카테고리 별 데이터 정제 후 .json & .xlsx로 저장
+# <Remarks>
+# 최대 100개의 데이터만 호출 가능
 def main():
+    sText = '메타버스'
+    dCount = 100
+    
     jsonDataResult1 = []
     jsonDataResult2 = []
     jsonDataResult3 = []
 
-    #sNode = ['news', 'blog', 'cafearticle']
-    sText = '메타버스'
-    dCount = 100
     jsonSearchResult1 = {}
-    jsonSearchResult2 = {}
+    jsonSearchResult2 = {}  
     jsonSearchResult3 = {}
 
-    #for node in sNode:
-    jsonSearchResult1= GetNaverSearchResult('news', sText, 1, dCount)
-    jsonSearchResult2= GetNaverSearchResult('blog', sText, 1, dCount)
-    jsonSearchResult3 =GetNaverSearchResult('cafearticle', sText, 1, dCount)
+    jsonSearchResult1 = GetNaverSearchResult('news', sText, 1, dCount)
+    jsonSearchResult2 = GetNaverSearchResult('blog', sText, 1, dCount)
+    jsonSearchResult3 = GetNaverSearchResult('cafearticle', sText, 1, dCount)
 
-    #if node == "news":
     for data in jsonSearchResult1['items']:
              GetNewsDateChange(data, jsonDataResult1)
-    #elif node == "blog":
     for data in jsonSearchResult2['items']:
              GetBlogDateChange(data, jsonDataResult2)
-    #elif node == "cafearticle":
     for data in jsonSearchResult3['items']:
              GetCafeDateChange(data, jsonDataResult3)
 
-    with open('통합연습2.json', 'w', encoding= 'utf-8') as filedata:
+    with open('메타버스.json', 'w', encoding= 'utf-8') as filedata:
        # if 'items' in jsonSearchResult1.keys():
-            # tempData = pd.DataFrame(jsonSearchResult['items'][0]['title']['pubDate']['description']['link']['originallink'])
-            News = pd.DataFrame(jsonDataResult1, columns= ['제목', '내용','날짜', 'link', 'originallink'])
-            Blog = pd.DataFrame(jsonDataResult2, columns= ['제목', '내용','날짜', 'link'])
-            Cafe = pd.DataFrame(jsonDataResult3, columns= ['제목', '내용', 'link'])
-    with pd.ExcelWriter('메타.xlsx') as writer: 
-                News.to_excel(writer, sheet_name = 'News')
-                Blog.to_excel(writer, sheet_name = 'Blog')
-                Cafe.to_excel(writer, sheet_name = 'Cafe')
-            #temp = temp.to_json(force_ascii=False, indent=4)
-        # rjson = json.dumps(jsonTotalData, indent=4, sort_keys=True, ensure_ascii=False)
-        #filedata.write(temp)
+        # tempData = pd.DataFrame(jsonSearchResult['items'][0]['title']['pubDate']['description']['link']['originallink'])
+        News = pd.DataFrame(jsonDataResult1, columns= ['제목', '내용','날짜', 'link', 'originallink'])
+        Blog = pd.DataFrame(jsonDataResult2, columns= ['제목', '내용','날짜', 'link'])
+        Cafe = pd.DataFrame(jsonDataResult3, columns= ['제목', '내용', 'link'])
 
+        temp = News.to_json(force_ascii=False, indent=4) + Blog.to_json(force_ascii=False, indent=4) + Cafe.to_json(force_ascii=False, indent=4) 
+        filedata.write(temp)
+
+    with pd.ExcelWriter('메타버스.xlsx') as writer: 
+        News.to_excel(writer, sheet_name = 'News')
+        Blog.to_excel(writer, sheet_name = 'Blog')
+        Cafe.to_excel(writer, sheet_name = 'Cafe')   
+
+# <Summary>
+# Main
 if __name__ == '__main__':
     main() 
